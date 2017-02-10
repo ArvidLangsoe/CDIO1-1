4fd5f3c903f7f2c0a3f8b847b7476b6a94f42cfb
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-10 16:14:22 +0100

    Added a meathod the validates cprNumbers

--- CDIO1/src/entity/UserCreator.java
+++ CDIO1/src/entity/UserCreator.java
@@ -1,5 +1,53 @@
 package entity;
 
-public class UserCreator {
+import java.util.List;
 
+import dal.IUserDAO;
+
+public class UserCreator {
+	
+	private IUserDAO data;
+	
+	public UserCreator(IUserDAO data){
+		this.data=data;
+	}
+	
+	
+	public String createUser(String userName,String ini,String cpr,List<String> roles){
+		
+		
+		return null;
+	}
+	
+	
+	public boolean isCprValid(String cpr){
+		int[] validationNumber=new int[]{4,3,2,7,6,5,4,3,2};
+		int[] cprArray =new int[10];
+		
+		//Save the cpr number in an array.
+		for (int i=0;i<10;i++){
+			cprArray[i]=Integer.parseInt(cpr.substring(i, i));
+		}
+		
+		//Calculate the controle cifre:
+		int sum=0;
+		for(int i=0;i<9;i++){
+			sum+=cprArray[i]*validationNumber[i];
+		}
+		
+		int remainder= (sum%11);
+		
+		if(remainder==1){
+			return false;
+		}
+		
+		int controlCifre = 11-remainder;
+		
+		if(controlCifre!=cprArray[9]){
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
