548fa90a78bc56c383f5012fefb34c14d75d96f5
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-10 18:10:04 +0100

    Added method to check if a new password is valid according to standards

--- CDIO1/src/entity/UserCreator.java
+++ CDIO1/src/entity/UserCreator.java
@@ -3,51 +3,122 @@ package entity;
 import java.util.List;
 
 import dal.IUserDAO;
+import entity.IFun.InputException;
 
 public class UserCreator {
-	
+
 	private IUserDAO data;
-	
-	public UserCreator(IUserDAO data){
-		this.data=data;
+
+	public UserCreator(IUserDAO data) {
+		this.data = data;
 	}
-	
-	
-	public String createUser(String userName,String ini,String cpr,List<String> roles){
-		
-		
+
+	public String createUser(String userName, String ini, String cpr, List<String> roles) {
+
 		return null;
 	}
-	
-	
-	public boolean isCprValid(String cpr){
-		int[] validationNumber=new int[]{4,3,2,7,6,5,4,3,2};
-		int[] cprArray =new int[10];
-		
-		//Save the cpr number in an array.
-		for (int i=0;i<10;i++){
-			cprArray[i]=Integer.parseInt(cpr.substring(i, i));
-		}
-		
-		//Calculate the controle cifre:
-		int sum=0;
-		for(int i=0;i<9;i++){
-			sum+=cprArray[i]*validationNumber[i];
-		}
-		
-		int remainder= (sum%11);
-		
-		if(remainder==1){
+
+	public String generatePassword() {
+
+		return null;
+	}
+
+	public boolean isPasswordValid(String password) {
+		boolean smallLetterFlag = false;
+		boolean bigLetterFlag = false;
+		boolean numberFlag = false;
+		boolean specialCharFlag = false;
+		int groupCount = 0;
+		char currentChar;
+		int charValue;
+
+		if (password.length() < 6) {
 			return false;
 		}
-		
-		int controlCifre = 11-remainder;
-		
-		if(controlCifre!=cprArray[9]){
+		for (int i = 0; i < password.length(); i++) {
+			currentChar = password.charAt(i);
+			charValue = Character.getNumericValue(currentChar);
+
+			// Is the char a small letter?
+			if (charValue >= 97 && charValue <= 122) {
+				if (smallLetterFlag == false) {
+					groupCount++;
+					smallLetterFlag = true;
+				}
+			}
+			// Is the char a big letter?
+			else if (charValue >= 65 && charValue <= 90) {
+				if (bigLetterFlag == false) {
+					groupCount++;
+					bigLetterFlag = true;
+				}
+			}
+			// is the char a number?
+			else if (charValue >= 48 && charValue <= 57) {
+				if (numberFlag == false) {
+					groupCount++;
+					numberFlag = true;
+				}
+			}
+			// Is the char an allowed special character.
+			else if (currentChar == '.' || currentChar == '-' || currentChar == '_' || currentChar == '+'
+					|| currentChar == '!' || currentChar == '?' || currentChar == '=') {
+				if (specialCharFlag == false) {
+					groupCount++;
+					specialCharFlag = true;
+				}
+			} else {
+				return false;
+			}
+
+		}
+
+		if (groupCount < 3) {
 			return false;
 		}
-		
+
+		return true;
+
+	}
+
+	public boolean isCprValid(String cpr) throws InputException {
+		int[] validationNumber = new int[] { 4, 3, 2, 7, 6, 5, 4, 3, 2 };
+		int[] cprArray = new int[10];
+
+		if (cpr.length() != 10) {
+			throw new InputException("The cpr number is not long enough.");
+		}
+
+		// Save the cpr number in an array.
+		for (int i = 0; i < 10; i++) {
+			cprArray[i] = Integer.parseInt(cpr.substring(i, i));
+		}
+		if (cprArray[0] > 3) {
+			throw new InputException(cprArray[0] + "" + cprArray[1] + " is not a valid date.");
+		}
+		if ((cprArray[2] > 2 || (cprArray[2] > 0 && cprArray[3] > 2))) {
+			throw new InputException(cprArray[2] + "" + cprArray[3] + "is not a valid month.");
+		}
+
+		// Calculate the controle cifre:
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			sum += cprArray[i] * validationNumber[i];
+		}
+
+		int remainder = (sum % 11);
+
+		if (remainder == 1) {
+			throw new InputException("The cpr number is invalid.");
+		}
+
+		int controlCifre = 11 - remainder;
+
+		if (controlCifre != cprArray[9]) {
+			throw new InputException("The cpr number is invalid.");
+		}
+
 		return true;
 	}
-	
+
 }
