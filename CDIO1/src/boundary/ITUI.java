037f907d8708fa928dc4f581fa74b5650c4aa8e6
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-10 21:12:11 +0100

    Added a interface for TUI's

---
+++ CDIO1/src/boundary/ITUI.java
@@ -0,0 +1,8 @@
+package boundary;
+
+public interface ITUI {
+	// Gets a string input from the user.
+	public String getUserInput();
+	// Shows the answer from the system to the user.
+	public void showResponse(String response);
+}
