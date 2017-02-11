a74b1b15f4d1029ae652b5e4058d2d1c166aad0d
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-11 23:15:14 +0100

    Added one test example.

--- CDIO1/src/test/userCreation/UserCreationControllerTest.java
+++ CDIO1/src/test/userCreation/UserCreationControllerTest.java
@@ -14,34 +14,38 @@ public class UserCreationControllerTest {
 	private ITUI tui;
 	private IUserCreation userCreator;
 	private UserCreationController con;
-	
 
 	@Before
 	public void setUp() throws Exception {
-		String[] validRoles=new String[]{"Role1,Role2,Role3,Role4"};
-		userCreator=new UserCreatorStub(validRoles);
+		String[] validRoles = new String[] { "Role1,Role2,Role3,Role4" };
+		userCreator = new UserCreatorStub(validRoles);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		tui=null;
-		userCreator=null;
-		con=null;
+		tui = null;
+		userCreator = null;
+		con = null;
 	}
 
 	@Test
 	public void testStartUserCreationNoExceptions() {
-		String[] expectedAnswers=new String[]{"User creation has begun."};
-		String[] userInputs=new String[]{"10","name","ini","cpr","1","5"};
-		
-		tui=new TUIStub(userInputs);
-		con= new UserCreationController(userCreator,tui);
+		String[] expectedAnswers = new String[] { "User creation has begun." };
+		String[] userInputs = new String[] { "10", "name", "ini", "cpr", "1", "5" };
+
+		tui = new TUIStub(userInputs);
+		TUIStub tuiTest = (TUIStub) tui;
+		con = new UserCreationController(userCreator, tui);
+
+		for (int i = 0; i < expectedAnswers.length; i++) {
+			assertEquals("The messages: '" + expectedAnswers[i] + "' and '" + tuiTest.responses.get(i)
+					+ " were not the same.", expectedAnswers[i], tuiTest.responses.get(i));
+		}
 	}
-	
-	
+
 	@Test
 	public void testStartUserCreationWithExceptions() {
-		
+
 	}
 
 }
