46137cf92eddd638e761ad7a0d2bbb82d21e8112
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-11 22:23:55 +0100

    Added generate password to UserDTO

--- CDIO1/src/dto/UserDTO.java
+++ CDIO1/src/dto/UserDTO.java
@@ -249,6 +249,52 @@ public class UserDTO implements Serializable {
 		return true;
 
 	}
+	public String generateValidPassword() {
+		String password = "";
+
+		int passLength = 8;
+		boolean passwordValid = false;
+		while (!passwordValid) {
+			password = "";
+			for (int i = 0; i < passLength; i++) {
+				char newCharacther;
+				int randGroup = (int) (Math.random() * 100);
+
+				// Add a special characther
+				if (randGroup < 5) {
+					String specialCharacthers = ".-_+!?=";
+					int rand = (int) (Math.random() * specialCharacthers.length());
+					newCharacther = specialCharacthers.charAt(rand);
+				}
+				// Add a small letter.
+				else if (randGroup < 30) {
+					int rand = (int) (Math.random() * (122 - 97 + 1) + 97);
+					newCharacther = (char) rand;
+
+				}
+				// Add a large letter.
+				else if (randGroup < 55) {
+					int rand = (int) (Math.random() * (90 - 65 + 1) + 65);
+					newCharacther = (char) rand;
+				}
+				// Add a number.
+				else {
+					int rand = (int) (Math.random() * (57 - 48 + 1) + 48);
+					newCharacther = (char) rand;
+				}
+
+				password += newCharacther + "";
+			}
+			try {
+				isPasswordValid(password);
+				passwordValid = true;
+			} catch (InputException e) {
+
+			}
+		}
+
+		return password;
+	}
 
 	public List<String> getRoles() {
 		return roles;
--- CDIO1/src/entity/UserCreator.java
+++ CDIO1/src/entity/UserCreator.java
@@ -56,58 +56,13 @@ public class UserCreator {
 
 	public String endUserCreation() throws InputException,DALException {
 
-		newUser.setPassword(generatePassword());
+		newUser.setPassword(newUser.generateValidPassword());
 
 		data.createUser(newUser);
 
 		return "Succes.";
 	}
 
-	private String generatePassword() {
-		String password = "";
-
-		int passLength = 8;
-		boolean passwordValid = false;
-		while (!passwordValid) {
-			password = "";
-			for (int i = 0; i < passLength; i++) {
-				char newCharacther;
-				int randGroup = (int) (Math.random() * 100);
-
-				// Add a special characther
-				if (randGroup < 5) {
-					String specialCharacthers = ".-_+!?=";
-					int rand = (int) (Math.random() * specialCharacthers.length());
-					newCharacther = specialCharacthers.charAt(rand);
-				}
-				// Add a small letter.
-				else if (randGroup < 30) {
-					int rand = (int) (Math.random() * (122 - 97 + 1) + 97);
-					newCharacther = (char) rand;
-
-				}
-				// Add a large letter.
-				else if (randGroup < 55) {
-					int rand = (int) (Math.random() * (90 - 65 + 1) + 65);
-					newCharacther = (char) rand;
-				}
-				// Add a number.
-				else {
-					int rand = (int) (Math.random() * (57 - 48 + 1) + 48);
-					newCharacther = (char) rand;
-				}
-
-				password += newCharacther + "";
-			}
-			try {
-				newUser.isPasswordValid(password);
-				passwordValid = true;
-			} catch (InputException e) {
-
-			}
-		}
-
-		return password;
-	}
+	
 
 }
