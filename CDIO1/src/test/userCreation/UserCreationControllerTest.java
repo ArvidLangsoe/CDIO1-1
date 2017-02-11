84033f292e6810c8ccdf1e06a0a86b3444b22b24
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-11 22:34:13 +0100

    Added 2 new test classes for the user creation use case

---
+++ CDIO1/src/test/userCreation/UserCreationControllerTest.java
@@ -0,0 +1,24 @@
+package test.userCreation;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserCreationControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStartUserCreation() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ CDIO1/src/test/userCreation/UserCreatorTest.java
@@ -0,0 +1,59 @@
+package test.userCreation;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserCreatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testStartUserCreation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIni() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCpr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddRole() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValidroles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEndUserCreation() {
+		fail("Not yet implemented");
+	}
+
+}
