812503a5108b555fbe7b075e001fb907737786e5
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-02-11 22:39:22 +0100

    Added a tui stub with various inputs.

--- CDIO1/src/test/userCreation/TUIStub.java
+++ CDIO1/src/test/userCreation/TUIStub.java
@@ -1,5 +1,25 @@
 package test.userCreation;
 
-public class TUIStub {
+import boundary.ITUI;
+
+public class TUIStub implements ITUI{
+
+	private String[] userInputs;
+	private int index;
+	
+	public TUIStub(String[] userInputs){
+		this.userInputs=userInputs;
+		index=0;
+		
+	}
+	@Override
+	public String getUserInput() {
+		return userInputs[index++];
+	}
+
+	@Override
+	public void showResponse(String response) {
+		
+	}
 
 }
