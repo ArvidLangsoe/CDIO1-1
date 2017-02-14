22fff26fa7e5a4b7e6d73d75cfbd378a3cdcaf66
Mads <mads@stege.eu>
2017-02-14 14:54:10 +0100

    WIP Updating classes

--- CDIO1/src/entity/UserEditor.java
+++ CDIO1/src/entity/UserEditor.java
@@ -3,10 +3,11 @@ package entity;
 import dal.UserDAO;
 import dto.UserDTO;
 import controller.UserEditorController;
+import boundary.interfaces.IUserEditorBoundary;
 import dto.InputException;
 import dal.IUserDAO.DALException;
 
-public class UserEditor {
+public class UserEditor implements IUserEditorBoundary {
 
 	private UserDAO userDAO;
 	private UserEditorController userEditorController;
@@ -17,37 +18,42 @@ public class UserEditor {
 
 	}
 
-	public void changeUserName(int userId, String changedUserName) throws DALException, InputException {
-		try {
-			UserDTO user = userDAO.getUser(userId);
+	public void changeUserName(UserDTO userDTO) {
+		while (true) {
 
-			user.setUserName(changedUserName);
+			try {
+				System.out.println("Please list new username:");
 
-			userDAO.updateUser(user);
+				String changedUserName = tui.getString;
 
-			System.out.println("Username updated to " + user.getUserName());
+				userDTO.setUserName(changedUserName);
 
-		} catch (DALException e) {
-			System.out.println("There exists no user with the specified user ID: " + userId);
-		} catch (InputException e) {
-			System.out.println("Input not recognised.");
+				System.out.println("Username updated to " + userDTO.getUserName());
+				return;
+			} catch (DALException e) {
+				System.out.println("There exists no user with the specified user: " + userDTO);
+			} catch (InputException e) {
+				System.out.println(e.getMessage());
+			}
 		}
 	}
 
-	public void changeUserIni(int userId, String changedUserIni) throws DALException, InputException {
-		try {
-			UserDTO user = userDAO.getUser(userId);
+	public void changeUserIni(UserDTO userDTO) {
+		while (true) {
+			try {
+				System.out.println("Please list new user Initials (2-4 characters long): ");
 
-			user.setIni(changedUserIni);
+				String changedUserIni = tui.getString;
 
-			userDAO.updateUser(user);
+				userDTO.setIni(changedUserIni);
 
-			System.out.println("Username updated to " + user.getUserName());
+				System.out.println("Username updated to " + userDTO.getUserName());
 
-		} catch (DALException e) {
-			System.out.println("There exists no user with the specified user ID: " + userId);
-		} catch (InputException e) {
-			System.out.println("Input not recognised.");
+			} catch (DALException e) {
+				System.out.println("There exists no user with the specified user ID: " + userDTO);
+			} catch (InputException e) {
+				System.out.println(e.getMessage());
+			}
 		}
 	}
 
@@ -70,16 +76,16 @@ public class UserEditor {
 	public void changeUserPassword(int userId, String ChangedUserPassword) {
 		try {
 			UserDTO user = userDAO.getUser(userId);
-				
+
 			user.setPassword(ChangedUserPassword);
-		
+
 			userDAO.updateUser(user);
 		} catch (DALException e) {
 			System.out.println("THere exists no user with the specified user ID: " + userId);
 		} catch (InputException e) {
 			System.out.println("Input not recognised.");
 		}
-		
+
 	}
 
 	public void removeUserRoles(int userId) throws DALException, InputException {
@@ -126,5 +132,60 @@ public class UserEditor {
 		} catch (InputException e) {
 			System.out.println("Input not recognised.");
 		}
+	}
+
+	@Override
+	public int getId() {
+		while (true) {
+			try {
+				System.out.println("Please specify wished userId");
+
+				UserDTO fetchedUser = new UserDTO();
+
+				int userInput = tui.getInt();
+
+				fetchedUser.isUserIDValid(userInput);
+
+				return userInput;
+			}
+
+			catch (InputException e) {
+				System.out.println("Input not recognised.");
+			}
+		}
+
+	}
+
+	@Override
+	public UserDTO editUser(UserDTO userDTO) {
+
+		System.out.println(
+				"Please choose an option: \n" + "1: Change the username. \n" + "2: Change the user's initials. \n"
+						+ "3: Change the user's password. \n" + "4: Manipulate user roles.");
+
+		int userChoice = tui.getInt;
+
+		switch (userChoice) {
+		case 1:
+			changeUserName(userDTO);
+			break;
+
+		case 2:
+			changeUserIni(userDTO);
+			break;
+
+		case 3:
+
+			break;
+
+		case 4:
+
+			break;
+
+		case 5:
+
+			break;
+		}
+		return null;
 	}
 }
