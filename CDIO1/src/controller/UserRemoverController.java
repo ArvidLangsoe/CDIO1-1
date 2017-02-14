077c4ae3bf123dfabe895559b22994a21b2e36f4
Peter <peterxp@hotmail.fr>
2017-02-14 07:52:00 +0100

    Simple update

--- CDIO1/src/controller/UserRemoverController.java
+++ CDIO1/src/controller/UserRemoverController.java
@@ -1,5 +1,35 @@
 package controller;
 
-public class UserRemoverController {
+import boundary.ITUI;
+import dal.IUserDAO;
+import entity.UserRemover;
 
+public class UserRemoverController {
+	UserRemover func;
+	ITUI tui;
+	IUserDAO data;
+
+	public UserRemoverController(IUserDAO data){
+		this.data=data;
+		func = new UserRemover(data);
+	}
+
+	public void start(){
+		while(!deleteUser(tui.getUserInput())){
+			tui.showResponse("Enter userID: ");
+		}
+	}
+
+	public boolean deleteUser(String input){
+		try{
+			int userID = Integer.parseInt(input);
+			func.deleteUser(userID);
+			return true;
+
+		}catch (Exception e){
+			tui.showResponse("ERROR: "+e);
+		}
+
+		return false;
+	}
 }
