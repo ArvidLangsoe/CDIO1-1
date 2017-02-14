6bde05670af705c939100e6fe5092f408436a831
Peter <peterxp@hotmail.fr>
2017-02-14 11:59:12 +0100

    UserDeletetion implemented

---
+++ CDIO1/src/boundary/UserDeletetionBoundary.java
@@ -0,0 +1,31 @@
+package boundary;
+
+import boundary.interfaces.IUserDeletetionBoundary;
+import boundary.interfaces.UI;
+
+public class UserDeletetionBoundary implements IUserDeletetionBoundary {
+	UI tui;
+	
+	public UserDeletetionBoundary(){
+		tui = new TUI();
+	}
+	
+	@Override
+	public int getId(){
+		
+		tui.show("Please enter a UserID: ");
+		
+		int id = tui.getInt();
+
+		while(11 <= id && id <= 99){
+			id = tui.getInt();
+			
+			tui.show("Incorrect ID. Must be between 11 and 99.\n Please enter a UserID");
+		}
+
+		return id;
+	}
+	
+	
+
+}
--- CDIO1/src/controller/UserRemoverController.java
+++ CDIO1/src/controller/UserRemoverController.java
@@ -1,35 +1,20 @@
 package controller;
 
-import boundary.ITUI;
+import boundary.UserDeletetionBoundary;
+import controller.interfaces.IUserDeletionController;
 import dal.IUserDAO;
-import entity.UserRemover;
+import dal.IUserDAO.DALException;
 
-public class UserRemoverController {
-	UserRemover func;
-	ITUI tui;
+public class UserRemoverController implements IUserDeletionController {
+	UserDeletetionBoundary boundary;
 	IUserDAO data;
 
 	public UserRemoverController(IUserDAO data){
 		this.data=data;
-		func = new UserRemover(data);
 	}
 
-	public void start(){
-		while(!deleteUser(tui.getUserInput())){
-			tui.showResponse("Enter userID: ");
-		}
-	}
-
-	public boolean deleteUser(String input){
-		try{
-			int userID = Integer.parseInt(input);
-			func.deleteUser(userID);
-			return true;
-
-		}catch (Exception e){
-			tui.showResponse("ERROR: "+e);
-		}
-
-		return false;
+	@Override
+	public void deleteUser() throws DALException{
+		data.deleteUser(boundary.getId());
 	}
 }
