ad84d348aa0e8495b3a94adb15285edb43de5f84
Mads <Mads@Mads-PC>
2017-02-15 19:07:51 +0100

    Added a manipulateUserRoles class

--- CDIO1/src/entity/UserEditor.java
+++ CDIO1/src/entity/UserEditor.java
@@ -112,49 +112,64 @@ public class UserEditor implements IUserEditorBoundary {
 	public void manipulateUserRoles(UserDTO userDTO) {
 		while (true) {
 			try {
-				System.out.println(
-						"Please select how you wish to manipulate user roles for " + userDTO.getUserName() + ". \n"
-								+ "");
+				System.out.println("Please select how you wish to manipulate user roles for " + userDTO.getUserName()
+						+ ". \n" + "1: Add a new role. \n" + "2: Remove a role. \n" + "3: Remove *ALL* roles."
+						+ "4: Exit role manipulator");
 				int playerChoice = tui.getInt;
-				
+
 				switch (playerChoice) {
 				case 1:
-					userDTO.removeAllRoles();
-					System.out.println("All of " + userDTO.getUserName() + "'s roles have been removed.");
+					System.out.println("Preparing to add role to user...");
+					addUserRole(userDTO);
 					break;
 
 				case 2:
-					System.out.println("No roles have been removed from " + userDTO.getUserName());
+					System.out.println("Preparing to remove one role from user...");
+					removeOneUserRole(userDTO);
+					break;
+
+				case 3:
+					System.out.println("Preparing to remove all roles from user...");
+					removeUserRoles(userDTO);
+					break;
+
+				case 4:
+					System.out.println("Exiting role manipulator");
+					editUser(userDTO);
 					break;
 				}
-				
+
 			} catch (DALException e) {
 				System.out.println("No user exists with the specified user ID.");
 			}
 		}
 	}
 
-	public void removeUserRoles(UserDTO userDTO) {
+	public void addUserRole(UserDTO userDTO) {
 
 		while (true) {
 			try {
-				System.out.println("Are you sure you want to remove *ALL* roles for " + userDTO.getUserName()
-						+ "? This choice is not revertable. \n" + "Type 1 for Yes. \n" + "Type 2 for No.");
+				System.out.println("Are you sure you want to add a role to " + userDTO.getUserName() + "? \n"
+						+ "Type 1 for Yes. \n" + "Type 2 for No.");
 
 				int playerChoice = tui.getInt;
 
 				switch (playerChoice) {
 				case 1:
-					userDTO.removeAllRoles();
-					System.out.println("All of " + userDTO.getUserName() + "'s roles have been removed.");
+					System.out.println("Please list the desired role to be added to " + userDTO.getUserName()
+							+ ": Admin, Pharmacist, Foreman or Operator.");
+					String playerChoice = tui.getString;
+					userDTO.addRole(playerChoice);
+					System.out.println(
+							"Added " + playerChoice + " to the list of " + userDTO.getUserName() + "'s roles.");
 					break;
 
 				case 2:
-					System.out.println("No roles have been removed from " + userDTO.getUserName());
-					break;
+					System.out.println("No roles added to " + userDTO.getUserName());
 				}
-
 			} catch (DALException e) {
+				System.out.println("No user exists with the specified user ID.");
+			} catch (InputException e) {
 				System.out.println(e.getMessage());
 			}
 		}
@@ -190,31 +205,27 @@ public class UserEditor implements IUserEditorBoundary {
 		}
 	}
 
-	public void addUserRole(UserDTO userDTO) {
+	public void removeUserRoles(UserDTO userDTO) {
 
 		while (true) {
 			try {
-				System.out.println("Are you sure you want to add a role to " + userDTO.getUserName() + "? \n"
-						+ "Type 1 for Yes. \n" + "Type 2 for No.");
+				System.out.println("Are you sure you want to remove *ALL* roles for " + userDTO.getUserName()
+						+ "? This choice is not revertable. \n" + "Type 1 for Yes. \n" + "Type 2 for No.");
 
 				int playerChoice = tui.getInt;
 
 				switch (playerChoice) {
 				case 1:
-					System.out.println("Please list the desired role to be added to " + userDTO.getUserName()
-							+ ": Admin, Pharmacist, Foreman or Operator.");
-					String playerChoice = tui.getString;
-					userDTO.addRole(playerChoice);
-					System.out.println(
-							"Added " + playerChoice + " to the list of " + userDTO.getUserName() + "'s roles.");
+					userDTO.removeAllRoles();
+					System.out.println("All of " + userDTO.getUserName() + "'s roles have been removed.");
 					break;
 
 				case 2:
-					System.out.println("No roles added to " + userDTO.getUserName());
+					System.out.println("No roles have been removed from " + userDTO.getUserName());
+					break;
 				}
+
 			} catch (DALException e) {
-				System.out.println("No user exists with the specified user ID.");
-			} catch (InputException e) {
 				System.out.println(e.getMessage());
 			}
 		}
@@ -245,9 +256,9 @@ public class UserEditor implements IUserEditorBoundary {
 	@Override
 	public UserDTO editUser(UserDTO userDTO) {
 
-		System.out.println(
-				"Please choose an option: \n" + "1: Change the username. \n" + "2: Change the user's initials. \n"
-						+ "3: Change the user's password. \n" + "4: Manipulate user roles.");
+		System.out.println("Please choose an option: \n" + "1: Change the username. \n"
+				+ "2: Change the user's initials. \n" + "3: Change the user's password. \n"
+				+ "4: Manipulate user roles. \n" + "5: Exit the User Editor.");
 
 		int userChoice = tui.getInt;
 
@@ -265,11 +276,10 @@ public class UserEditor implements IUserEditorBoundary {
 			break;
 
 		case 4:
-
+			manipulateUserRoles(userDTO);
 			break;
 
 		case 5:
-
 			break;
 		}
 		return null;
