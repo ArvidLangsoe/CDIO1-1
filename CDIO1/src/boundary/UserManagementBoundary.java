1e5ed80a824f6ad66f9a5e4a03dd7f0ee5fab4d5
Peter <peterxp@hotmail.fr>
2017-02-16 15:13:19 +0100

    Updating UserManagementBoundary

--- CDIO1/src/boundary/UserManagementBoundary.java
+++ CDIO1/src/boundary/UserManagementBoundary.java
@@ -1,23 +1,63 @@
 package boundary;
 
-import boundary.interfaces.IUserManagementBoundary;
-import boundary.interfaces.UI;
+import boundary.interfaces.*;
 
-public class UserManagementBoundary implements IUserManagementBoundary {
-
-	int choice;
-	private UI TUI;
-	public UserManagementBoundary(UI TUI) {
-		this.TUI = TUI;
+public class UserManagementBoundary extends TUI implements IUserManagementBoundary {
+	
+	private IUserCreationBoundary create;
+	private IUserDeletetionBoundary delete;
+	private IUserInformationBoundary information;
+	private IUserEditorBoundary edit;
+	
+	String[] options = { "1:Opret ny bruger", "2:List Brugere", "3:Ret bruger", "4:Slet bruger", "5:Afslut program" };
+	boolean run = true;
+	
+	public UserManagementBoundary(IUserCreationBoundary create, IUserDeletetionBoundary delete, IUserInformationBoundary information,IUserEditorBoundary edit) {
+		this.create = create;
+		this.delete = delete;
+		this.information = information;
+		this.edit = edit;
 	}
 
-	public int getUserChoice(String[] options){
-		for(int i=0;i<options.length;i++)
+	public int getUserChoice(){
+		for(int i=0 ; i < options.length ; i++)
 		{
-			TUI.show(options[i]);
+			show(options[i]);
 		}
-		choice = TUI.getInt();
-		return choice;		
+
+		return getInt();
 	}
 
+
+	public void manageUsers() {
+		
+	}
+
+	public void choiceHandler(int decision) {
+		switch (decision) {
+		case 1:
+			create.createNewUser();
+			break;
+		case 4:
+			delete.deleteUser();
+			break;
+		case 2:
+			information.showUsers();
+			break;
+		case 3:
+			edit.editUser();
+			break;
+		case 5:
+			run = false;
+			break;
+		}
+
+	}
+
+	public void start() {
+		while (run) {
+			choiceHandler(getUserChoice());
+		}
+	}
+	
 }
