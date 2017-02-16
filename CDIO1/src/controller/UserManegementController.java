8f74f060831e7cb3d17deacd9d51f8bfcfacd3d5
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-16 13:22:33 +0100

    Added break to case.

--- CDIO1/src/controller/UserManegementController.java
+++ CDIO1/src/controller/UserManegementController.java
@@ -7,8 +7,6 @@ import controller.interfaces.IUserInformationController;
 import controller.interfaces.IUserManagementController;
 import boundary.interfaces.IUserManagementBoundary;
 
-
-
 public class UserManegementController implements IUserManagementController {
 
 	private IUserCreationController create;
@@ -16,54 +14,50 @@ public class UserManegementController implements IUserManagementController {
 	private IUserInformationController information;
 	private IUserEditorController edit;
 	private IUserManagementBoundary TUIBound;
-	public UserManegementController(
-			IUserCreationController create,
-			IUserDeletionController delete,
-			IUserInformationController information,
-			IUserEditorController edit,
-			IUserManagementBoundary TUIBound) {
-		this.create=create;
-		this.delete=delete;
-		this.information=information;
-		this.edit=edit;
-		this.TUIBound =TUIBound;
+
+	public UserManegementController(IUserCreationController create, IUserDeletionController delete,
+			IUserInformationController information, IUserEditorController edit, IUserManagementBoundary TUIBound) 
+	{
+		this.create = create;
+		this.delete = delete;
+		this.information = information;
+		this.edit = edit;
+		this.TUIBound = TUIBound;
 	}
 
 	@Override
 	public void manageUsers() {
 
 	}
-	public void choiceHandler(int decision)
-	{
-		switch(decision)
-		{
-		case 1: create.createUser();
-		case 4: delete.deleteUser();
-		case 2: information.getUserInformation();
-		case 3: edit.editUser();
-		case 5: run = false;
+
+	public void choiceHandler(int decision) {
+		switch (decision) {
+		case 1:
+			create.createUser();
+			break;
+		case 4:
+			delete.deleteUser();
+			break;
+		case 2:
+			information.getUserInformation();
+			break;
+		case 3:
+			edit.editUser();
+			break;
+		case 5:
+			run = false;
+			break;
 		}
-		
+
 	}
-	public void start()
-	{
-		while(run)
-		{
+
+	public void start() {
+		while (run) {
 			choiceHandler(TUIBound.getUserChoice(options));
-			
-
 		}
-
 	}
-	String[] options=
-		{
-				"1:Opret ny bruger",
-				"2:List Brugere",
-				"3:Ret bruger",
-				"4:Slet bruger",
-				"5:Afslut program"
-		};
-	boolean run = true;
 
+	String[] options = { "1:Opret ny bruger", "2:List Brugere", "3:Ret bruger", "4:Slet bruger", "5:Afslut program" };
+	boolean run = true;
 
 }
