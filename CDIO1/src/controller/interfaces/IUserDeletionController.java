a7d3220a288b249a0f6448c01e7b058df2a14292
simonengquist <simonengquist@simons-mbp.eduroam.wireless.dtu.dk>
2017-02-16 12:27:22 +0100

    removed an exception

--- CDIO1/src/controller/interfaces/IUserDeletionController.java
+++ CDIO1/src/controller/interfaces/IUserDeletionController.java
@@ -1,10 +1,10 @@
 package controller.interfaces;
 
-import dal.IUserDAO.DALException;
+
 
 public interface IUserDeletionController {
 
 	// Get a userId from the boundary and deletes the corresponding userDTO.
-	public void deleteUser() throws DALException;
+	public void deleteUser();
 	
 }
