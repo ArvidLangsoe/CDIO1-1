7d9f68e824f04a16d1d15bcc26216e905e708a90
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-19 18:26:31 +0100

    UI comment code.
    - Added class comment.
    - added getString comment.
    - Added getInt comment.
    - added show comment.

--- CDIO1/src/presentationClasses/UI.java
+++ CDIO1/src/presentationClasses/UI.java
@@ -1,9 +1,28 @@
 package presentationClasses;
 
+/**
+ * The user interface (UI) makes sure the boundaries either TUI or GUI have the 
+ * right methods to communicate with the rest of the program.
+ * @author Group 22
+ *
+ */
 public interface UI {
-	//
+	/**
+	 * returns a string.
+	 * @return The string to be returned.
+	 */
 	public String getString();
+	
+	/**
+	 * Returns an integer.
+	 * @return The integer to be returned.
+	 */
 	public int getInt();
+	
+	/**
+	 * Shows a messages.
+	 * @param msg The message to be showed.
+	 */
 	public void show(String msg);
 
 }
