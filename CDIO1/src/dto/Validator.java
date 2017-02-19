4fb4c022cb9c732e0ec8115a481f874464f0332c
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-19 16:39:40 +0100

    Validator code comments.
    - Added a comment to the constant of the class.
    - Added a comment to the validateUserID method.
    - Added a comment to the validateUsername method.
    - Added a comment to the validateInitials method.
    - Added a comment to the valitePassword.
    - Added a comment to the validateRole method
    - Added a class comment to the inner class.
    - Added a comment to the constructor in the inner class.
    - Added a class comment to the validator class.

--- CDIO1/src/dto/Validator.java
+++ CDIO1/src/dto/Validator.java
@@ -1,10 +1,23 @@
 package dto;
 
+/**
+ * The Validator class only contains static methods and is used to check if the user input is valid.
+ * The class also contains the inner class InputException which is an exception class. The exception 
+ * is thrown if the user input something wrong.
+ * @author Group 22
+ *
+ */
 public class Validator {
 	
-	
+	//Constant of the class. Contains the valid roles in the system
 	public static final String[] validRoles = new String[] { "Admin", "Pharmacist", "Foreman", "Operator" };
 	
+	/**
+	 * Checks if the given userID has the valid size. It does not check if it is in the database.
+	 * @param userID The userID to be checked.
+	 * @return True if the userID is valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given userID is not valid.
+	 */
 	public static boolean validateUserID(int userID) throws InputException {
 		if (userID < 11 || userID > 99) {
 			throw new InputException("This user id is invalid. User ID's has to be between 11 and 99");
@@ -12,7 +25,14 @@ public class Validator {
 		return true;
 	}
 	
+	/**
+	 * Checks if the given username has the valid size.
+	 * @param username The username to be checked.
+	 * @return True if the username is valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given username is not valid.
+	 */
 	public static boolean validateUsername(String username) throws InputException {
+		// TODO overflødig kode? det andet if checker jo netop dette??
 		if (username==null)
 		{
 			throw new InputException("username can't be null");
@@ -25,18 +45,33 @@ public class Validator {
 		return true;
 	}
 	
+	/**
+	 * Checks if the given initials has the valid size.
+	 * @param ini The initials to be checked.
+	 * @return True if the initials are valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given initials is not valid.
+	 */
 	public static boolean validateInitials(String ini) throws InputException {
+		// TODO overflødig kode? Det andet if checker jo netop dette?
 		if (ini==null)
 		{
 			throw new InputException("initials can't be null");
 		}
+		
 		if (ini.length() < 2 || ini.length() > 4) {
 			throw new InputException("These initials are invalid. Initials has to be between 2 and 4 characthers long.");
 		}
 		return true;
 	}
 	
+	/**
+	 * Checks if the given CPR is a valid CPR as we know it the CPR in Denmark.
+	 * @param cpr The CPR to be checked.
+	 * @return True if the CPR is valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given CPR is not valid.
+	 */
 	public static boolean validateCPR(String cpr) throws InputException {
+		// TODO Overflødig kode? Det andet if checker jo netop dette?
 		if (cpr==null)
 		{
 			throw new InputException("cpr can't be null");
@@ -87,6 +122,12 @@ public class Validator {
 		return true;
 	}
 	
+	/**
+	 * Checks if the given password is valid. A password is valid if it follows the requirements that DTU has set.
+	 * @param password The password to be checked.
+	 * @return True if the password is valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given password is not valid.
+	 */
 	public static boolean validatePassword(String password) throws InputException {
 		if (password==null)
 		{
@@ -151,11 +192,19 @@ public class Validator {
 		return true;
 	}
 	
+	/**
+	 * Checks if the given role is a valid role.
+	 * @param role The role to be checked.
+	 * @return True if the role is valid, throws InputException otherwise.
+	 * @throws InputException The exception to be thrown if the given role is not valid.
+	 */
 	public static boolean validateRole(String role) throws InputException {
+		// TODO Overflødig kode igen?
 		if (role==null)
 		{
 			throw new InputException("Roles can't be null");
 		}
+		
 		for (int i = 0; i < validRoles.length; i++) {
 			if (role.equals(validRoles[i])) {
 				return true;
@@ -164,11 +213,21 @@ public class Validator {
 		throw new InputException("This is not a valid role.");
 	}
 
-	//Inner class - Exception class.
+	/**
+	 * The InputException class is an exception inner class used in the Validator class.
+	 * The exception is thrown if the user inputs something other than told.
+	 * @author Group 22
+	 *
+	 */
 	public static class InputException extends Exception 
 	{
+		//The serial id making us able to identify the object when saved and loaded.
 		private static final long serialVersionUID = 7884927058176762594L;
 		
+		/**
+		 * Constructor.
+		 * @param msg The error message.
+		 */
 		public InputException(String msg)
 		{
 			super(msg);
