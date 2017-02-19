d1d1565430aa761a5c63dd99ca708c37f1b04846
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-19 18:31:28 +0100

    Corrected a few comments.

--- CDIO1/src/presentationClasses/UI.java
+++ CDIO1/src/presentationClasses/UI.java
@@ -2,25 +2,25 @@ package presentationClasses;
 
 /**
  * The user interface (UI) makes sure the boundaries either TUI or GUI have the 
- * right methods to communicate with the rest of the program.
+ * right methods to communicate with the rest of the program and the user.
  * @author Group 22
  *
  */
 public interface UI {
 	/**
-	 * returns a string.
+	 * returns a string from the user.
 	 * @return The string to be returned.
 	 */
 	public String getString();
 	
 	/**
-	 * Returns an integer.
+	 * Returns an integer from the user.
 	 * @return The integer to be returned.
 	 */
 	public int getInt();
 	
 	/**
-	 * Shows a messages.
+	 * Shows a messages to a user.
 	 * @param msg The message to be showed.
 	 */
 	public void show(String msg);
