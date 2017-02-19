0ff15d3188762ea5ad13b7d70bfa3fa15beb9ad8
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-19 19:10:15 +0100

    UserRemover
    - Added comments to all methods.
    - Added class comment.
    - Added other comments.

--- CDIO1/src/presentationClasses/UserRemover.java
+++ CDIO1/src/presentationClasses/UserRemover.java
@@ -4,13 +4,28 @@ package presentationClasses;
 import dal.IUserDAO;
 import dal.IUserDAO.DALException;
 
+/**
+ * The class UserRemover extends TUI.
+ * The responsibility of this class is to carry out the tasks of removing a user from the data.
+ * @author Group 22
+ *
+ */
 public class UserRemover extends TUI {
+	
+	//Instance variables.
 	private IUserDAO data;
 	
+	/**
+	 * Constructor.
+	 * @param data The data access object to use.
+	 */
 	public UserRemover(IUserDAO data){
 		this.data=data;
 	}
 	
+	/**
+	 * Deletes a user (UserDTO) by asking the user Administrator for user ID of the user to be removed from the data.
+	 */
 	public void deleteUser()
 	{
 		try
@@ -28,6 +43,10 @@ public class UserRemover extends TUI {
 		}
 	}
 	
+	/**
+	 * Asks the user administrator for the user ID of the user (UserDTO) to be removed from the data.
+	 * @return The user ID to be deleted.
+	 */
 	public int getId(){
 		
 		show("Please enter a UserID: ");
