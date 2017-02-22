f44945efa2b9bf58ca01ad5378926a7369b8ca74
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-22 15:47:39 +0100

    IDataVerifier
    - Took the interface from Develop.
    - Corrected package names.

--- CDIO1/src/functionLayer/IDataVerifier.java
+++ CDIO1/src/functionLayer/IDataVerifier.java
@@ -1,5 +1,12 @@
 package functionLayer;
 
-public interface IDataVerifier {
+import dataAccessObjects.IUserDAO.DALException;
+import dataTransferObjects.UserDTO;
+import functionLayer.DataVerifier.WrongDataException;
 
+public interface IDataVerifier {
+	public void createUser(UserDTO user) throws WrongDataException;
+	public UserDTO getUser(int userId) throws DALException;
+	public void updateUser(UserDTO user) throws WrongDataException;
 }
+
