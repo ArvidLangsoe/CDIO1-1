ffa719f057a71832d10733576cdfd6a616151706
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-23 14:30:19 +0100

    Corrected main comments.

--- CDIO1/src/Main/Main.java
+++ CDIO1/src/Main/Main.java
@@ -11,26 +11,30 @@ import presentationLayer.UserRemover;
 import presentationLayer.UserViewer;
 
 /**
- * The Main class has the responsibility of intializing the objects used by the program and afterwards call the start method.
+ * The Main class has the responsibility of initializing the objects used by the
+ * program and afterwards call the start method.
+ * 
  * @author Group 22
  *
  */
 public class Main {
 
 	/**
-	 * The main method initializes the objects needed to run the user 
+	 * The main method initializes the objects needed to run the user
 	 * Administration program and afterwards starts the program.
-	 * @param args is not used.
+	 * 
+	 * @param args
+	 *            is not used.
 	 */
 	public static void main(String[] args) {
-			IUserDAO data = new UserDAO(null);
-			IDataVerifier func= new DataVerifier(data);
-			UserViewer view = new UserViewer(data);
-			UserRemover delete = new UserRemover(data);
-			UserCreator create = new UserCreator(func);
-			UserEditor edit = new UserEditor(func);
-			MenuAdministrator menu = new MenuAdministrator(create, delete, view, edit);
-			menu.start();
+		IUserDAO data = new UserDAO(null);
+		IDataVerifier func = new DataVerifier(data);
+		UserViewer view = new UserViewer(data);
+		UserRemover delete = new UserRemover(data);
+		UserCreator create = new UserCreator(func);
+		UserEditor edit = new UserEditor(func);
+		MenuAdministrator menu = new MenuAdministrator(create, delete, view, edit);
+		menu.start();
 	}
 
 }
