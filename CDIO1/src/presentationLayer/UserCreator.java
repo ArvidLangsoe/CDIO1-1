9a4f1c02c8fdf5b9f1bc59cb48fcca0f90eb0b4d
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-02-23 14:49:33 +0100

    Corrected comments.

--- CDIO1/src/presentationLayer/UserCreator.java
+++ CDIO1/src/presentationLayer/UserCreator.java
@@ -7,16 +7,16 @@ import staticClasses.Validator;
 import staticClasses.Validator.InputException;
 
 /**
- * The class UserCreator extends the class TUI. 
- * The responsibility of this class is to carry out the tasks of creating a new user and add it to the data.
+ * The class UserCreator extends the class TUI. The responsibility of this class
+ * is to carry out the tasks of creating a new user and add it to the data.
+ * 
  * @author Group 22
  *
  */
 public class UserCreator extends TUI {
 
 	// Instance variables.
-	private UserDTO newUser; 
-	
+	private UserDTO newUser;
 	private IDataVerifier data;
 
 	/**
@@ -30,36 +30,35 @@ public class UserCreator extends TUI {
 	}
 
 	/**
-	 * Creates a new user (UserDTO) by asking the user administrator for the information required and generates the rest.
+	 * Creates a new user (UserDTO) by asking the user administrator for the
+	 * information required and generates the rest.
 	 */
 	public void createNewUser() {
 		newUser = new UserDTO();
-		
+
 		getUserID();
 		getUserName();
 		getCpr();
 		getRoles();
 
-
 		try {
 			data.createUser(newUser);
-			show("User: " + newUser.getUserName()+ " has been added.");
+			show("User: " + newUser.getUserName() + " has been added.");
 		} catch (WrongDataException e) {
 			show(e.getMessage());
 		}
-		
-		
 	}
 
 	/**
-	 * Asks the user administrator which userID should be assigned to the new user (UserDTO).
+	 * Asks the user administrator which userID should be assigned to the new
+	 * user (UserDTO).
 	 */
 	private void getUserID() {
-		String question="\n"+"Please enter the ID of the new user. It has to be between 11 and 99";
+		String question = "\n" + "Please enter the ID of the new user. It has to be between 11 and 99";
 		while (true) {
 			show(question);
 			try {
-				int userID=getInt();
+				int userID = getInt();
 				Validator.validateUserID(userID);
 				newUser.setUserId(userID);
 				break;
@@ -72,10 +71,11 @@ public class UserCreator extends TUI {
 	}
 
 	/**
-	 * Asks the user administrator which username should be assigned to the new user (UserDTO)
+	 * Asks the user administrator which username should be assigned to the new
+	 * user (UserDTO)
 	 */
 	private void getUserName() {
-		String question = "\n"+"Please enter a username. The username has to have a length between 2 and 20.";
+		String question = "\n" + "Please enter a username. The username has to have a length between 2 and 20.";
 		while (true) {
 			show(question);
 			String userInput = getString();
@@ -87,14 +87,14 @@ public class UserCreator extends TUI {
 				show(e.getMessage());
 			}
 		}
-
 	}
 
 	/**
-	 * Asks the user administrator which CPR should be assigned to the new user (UserDTO).
+	 * Asks the user administrator which CPR should be assigned to the new user
+	 * (UserDTO).
 	 */
 	private void getCpr() {
-		String question = "\n"+"Please enter the users cpr number.";
+		String question = "\n" + "Please enter the users cpr number.";
 		while (true) {
 			show(question);
 			String userInput = getString();
@@ -106,44 +106,43 @@ public class UserCreator extends TUI {
 				show(e.getMessage());
 			}
 		}
-
 	}
 
 	/**
-	 * Asks the user administrator which roles should be assigned to the new user (UserDTO).
+	 * Asks the user administrator which roles should be assigned to the new
+	 * user (UserDTO).
 	 */
 	private void getRoles() {
-		String question = "\n"+"What roles do you want to assign to the user? Enter a number corresponding to a role:";
+		String question = "\n"
+				+ "What roles do you want to assign to the user? Enter a number corresponding to a role:";
 		String[] validRoles = Validator.validRoles;
 		boolean[] chosenRoles = new boolean[validRoles.length];
 		for (int i = 0; i < chosenRoles.length; i++) {
 			chosenRoles[i] = false;
 		}
-
+		
 		while (true) {
 			String out = question;
 			for (int i = 1; i <= chosenRoles.length; i++) {
-				if (chosenRoles[i-1] == false) {
-					out += ("\n" + i + ": " + validRoles[i-1] + ".");
+				if (chosenRoles[i - 1] == false) {
+					out += ("\n" + i + ": " + validRoles[i - 1] + ".");
 				}
 			}
-			out += "\n" + (chosenRoles.length+1) + ": Stop selecting roles.";
+			out += "\n" + (chosenRoles.length + 1) + ": Stop selecting roles.";
 			try {
 				show(out);
 				int userInput = getInt();
-				if (userInput == chosenRoles.length+1) {
+				if (userInput == chosenRoles.length + 1) {
 					break;
-				}
-				else if (userInput > chosenRoles.length+1 || userInput < 0) {
+				} else if (userInput > chosenRoles.length + 1 || userInput < 0) {
 					show("That is not a valid choice.");
 				} else {
-					newUser.addRole(validRoles[userInput-1]);
-					chosenRoles[userInput-1] = true;
+					newUser.addRole(validRoles[userInput - 1]);
+					chosenRoles[userInput - 1] = true;
 				}
 			} catch (NumberFormatException e) {
 				show("That is not a number.");
 			}
 		}
-
 	}
 }
